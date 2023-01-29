2f2e6fe85443bd742edc1cf6158b2582ad50c5f0
Moxy <keanu.knighton@gmail.com>
2023-01-29 14:28:06 +0000

    Past Present Future functionality implemented

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -23,9 +23,26 @@ $(document).ready(function(){
 
     function hourUpdate(){
         let currentHour = moment().hours();
-console.log(currentHour);
+
+        for(let i=0; i < $('.time-block').length; i++){
+            let hour = parseInt($('.time-block')[i].getAttribute('id').split('-')[1])
+            if(hour < currentHour) {
+                $('.time-block')[i].classList.add('past')
+            } else if(hour === currentHour){
+                $('.time-block')[i].classList.add('present')
+            } else {
+                $('.time-block')[i].classList.add('future')
+            }
+        }
+
     }
 
     hourUpdate();
+
+    let interval = setInterval(hourUpdate, 15000);
+
+    $('hour-8.description').val(localStorage.getItem('hour-8'))
+
+    $('#currentDay').text(moment().format('dddd, MMMM, Do'))
 
 })
