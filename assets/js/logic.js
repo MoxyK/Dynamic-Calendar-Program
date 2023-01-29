e276838551bb69484112b04f2bed1c1dcb176a6a
Moxy <keanu.knighton@gmail.com>
2023-01-29 14:41:42 +0000

    Local storage functionality w/ timeBlocks

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
 
         setTimeout(function(){
             $('.notification').removeClass('show');
-        }, 3000);
+        }, 3500);
 
     })
         // Past/Present/Future function
@@ -37,12 +37,22 @@ $(document).ready(function(){
 
     }
 
-    hourUpdate();
-
+    hourUpdate()
+        
     let interval = setInterval(hourUpdate, 15000);
 
-    $('hour-8.description').val(localStorage.getItem('hour-8'))
+        $('#hour-8.description').val(localStorage.getItem('hour-8'))
+        $('#hour-9.description').val(localStorage.getItem('hour-9'))
+        $('#hour-10.description').val(localStorage.getItem('hour-10'))
+        $('#hour-11.description').val(localStorage.getItem('hour-11'))
+        $('#hour-12.description').val(localStorage.getItem('hour-12'))
+        $('#hour-13.description').val(localStorage.getItem('hour-13'))
+        $('#hour-14.description').val(localStorage.getItem('hour-14'))
+        $('#hour-15.description').val(localStorage.getItem('hour-15'))
+        $('#hour-16.description').val(localStorage.getItem('hour-16'))
+        $('#hour-17.description').val(localStorage.getItem('hour-17'))
 
     $('#currentDay').text(moment().format('dddd, MMMM, Do'))
 
 })
+console.log(localStorage);
